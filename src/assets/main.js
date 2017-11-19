b298b4081924778e8020c0bc3c36bd86dd9665c0
Stanislav Burov <ch0col8te@me.com>
2017-11-19 15:36:22 +0300

    Added AJAX Call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Ch0coL8te.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
 
 });
